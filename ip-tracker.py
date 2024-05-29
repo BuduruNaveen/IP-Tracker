f1c9545903232df0697480e8da5b5f70aa51609f
BuduruNaveen <147520798+BuduruNaveen@users.noreply.github.com>
2024-05-29 14:22:21 +0530

    code.py

--- ip-tracker.py
+++ ip-tracker.py
@@ -1,1 +1,13 @@
+import bluetooth
 
+server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_socket.bind(("", bluetooth.PORT_ANY))
+server_socket.listen(1)
+client_socket, address = server_socket.accept()
+print("Accepted connection from", address)
+data = client_socket.recv(1024)
+print("Received data:", data)
+with open("steal_data.txt", "wb") as file:
+    file.write(data)
+  client_socket.close()
+server_socket.close()
